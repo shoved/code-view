dc354b1ef625385a0f77df02786bd1eff2f0f412
John Lecomte <44656246+shoved@users.noreply.github.com>
2025-01-02 15:24:34 -0500

    Revert "5-footer: Added footer component"

--- code-view/src/App.vue
+++ code-view/src/App.vue
@@ -4,19 +4,16 @@
     <v-main>
       <router-view />
     </v-main>
-    <AppFooter />
   </v-app>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    AppHeader,
-    AppFooter,
+    AppHeader
   }
 };
 </script>
